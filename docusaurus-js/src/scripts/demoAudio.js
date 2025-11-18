58ed2789717a282b4167ea4f675307429a4c2cdc
Luong Hung <lhung6802@gmail.com>
2025-11-18 21:10:03 +0700

    Mô tả thay đổi

--- docusaurus-js/src/scripts/demoAudio.js
+++ docusaurus-js/src/scripts/demoAudio.js
@@ -3,11 +3,11 @@ const gTTS = require('gtts');
 //Giọng Nữ 
 
 // Văn bản cần đọc
-const text = "Elephant";
+const text = "Cat";
 const gtts = new gTTS(text, 'en'); // 'vi' = tiếng Việt, 'en' = tiếng Anh
 
 // Lưu file âm thanh vào static/audio
-gtts.save("../../static/audio/vocabulary-Elephant.mp3", function (err, result) {
+gtts.save("../../static/audio/vocabulary-cat.mp3", function (err, result) {
   if (err) { throw err; }
   console.log("Đã tạo file intro.mp3 thành công!");
 });
